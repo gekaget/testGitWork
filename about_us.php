ffa86c56935fbb7005498df7e3a49ff974975655
Ievgen <igetmanets83@gmail.com>
2018-06-04 16:18:03 +0300

    04_06_18 16:17 My commit work

---
+++ about_us.php
@@ -0,0 +1,239 @@
+<?php
+/*$_['text_main_title'] = 'Интернет - магазин "NATASHASOUL"';*/
+$_['heading_title'] = 'О нас';
+$_['text_home']          = 'Главная';
+
+$_['text_content'] = <<<EOD
+<section class="section section-about-brand service">
+    <div class="decor-element decor-element-1"></div>
+    <div class="decor-element decor-element-2"></div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-12 col-md-7 col-md-offset-5">
+                <div class="section-content-part">
+                     <p class="site-name-text">Natasha Soul</p>
+                     <div class="about-service-wrap">
+                        <h3 class="about-brand-head">Услуги</h3>
+                        <p class="about-brand-text">Значимость этих проблем настолько очевидна, что постоянное информационно обеспечение нашей деятельности играет важную роль в формировании систем массового участия.</p>
+                        <p class="about-brand-text">Идейные соображения высшего порядка, а также реализация намеченных плановых заданий позволяет выполнять важные задания по разработке позиций, занимаемых участниками в отношении поставленных задач. Повседневная практика показывает, что рамки и место обучения кадров играет важную роль в формировании системы обучения кадров, соответствует насущным потребностям.</p>
+                        <p class="about-brand-text">Не следует, однако забывать, что постоянное информационно обеспечение нашей деятельности позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям. Разнообразный и богатый опыт постоянный количественный рост и сфера нашей активности позволяет выполнять важные задания по разработке новых предложений.</p>
+                     </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="decor-img img-elem">
+        <img src="catalog/view/theme/natashasou/images/services-img_1.jpg" alt="service_img">
+    </div>
+</section>
+
+<section class="section section-about-brand studio">
+    <div class="decor-element decor-element-1"></div>
+    <div class="decor-element decor-element-2"></div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-12 col-md-5">
+                <div class="section-content-part">
+                     <p class="site-name-text">Ателье</p>
+                     <div class="about-service-wrap">
+                        <h3 class="aboutUs-title">Пошив одежды по эксклюзивным моделям от бренда</h3>
+                        <p class="about-brand-text">Значимость этих проблем настолько очевидна, что постоянное информационно обеспечение нашей деятельности играет важную роль в формировании систем массового участия. Идейные соображения высшего порядка, а также реализация намеченных плановых заданий позволяет выполнять важные задания по разработке позиций, занимаемых участниками в отношении поставленных задач. </p>
+                        <p class="about-brand-text">Равным образом реализация намеченных плановых заданий в значительной степени обуславливает создание направлений прогрессивного развития.</p>
+                     </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="decor-img img-elem">
+        <img src="catalog/view/theme/natashasou/images/services-img_2.jpg" alt="service_img">
+    </div>
+</section>
+
+<section class="section section-about-brand jobs">
+
+        <div class="jobs-content-part">
+
+             <div class="jobs-wrap">
+                <div class="job-img">
+                    <img src="catalog/view/theme/natashasou/images/job_img_1.jpg" alt="service_img">
+                </div>
+                <div class="job-img">
+                    <img src="catalog/view/theme/natashasou/images/job_img_1.jpg" alt="service_img">
+                </div>
+                <div class="job-img">
+                    <img src="catalog/view/theme/natashasou/images/job_img_1.jpg" alt="service_img">
+                </div>
+             </div>
+
+             <p class="site-name-text">Наши работы</p>
+
+        </div>
+
+</section>
+
+<section class="section section-about-brand collections">
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-sm-12 col-md-5 col-md-offset-1">
+                <div class="section-content-part">
+                     <div class="about-service-wrap">
+                        <h3 class="about-brand-head">Коллекции</h3>
+                        <p class="about-brand-text">С другой стороны новая модель организационной деятельности требуют от нас анализа модели развития. Идейные соображения высшего порядка, а также рамки и место обучения кадров требуют от нас анализа существенных финансовых и административных условий. Разнообразный и богатый опыт постоянный количественный рост и сфера нашей активности позволяет выполнять важные задания по разработке новых предложений.</p>
+                        <p class="about-brand-text">Равным образом реализация намеченных плановых заданий требуют определения и уточнения направлений прогрессивного развития. Товарищи! новая модель организационной деятельности представляет собой интересный эксперимент проверки соответствующий условий активизации. Повседневная практика показывает, что рамки и место обучения кадров играет важную роль в формировании системы обучения кадров, соответствует насущным потребностям.</p>
+                        <div class="btn-wrap">
+                            <button class="btn btn_read_more">
+                                <span class="btn_text">Подробнее</span>
+                                <i class="icon-slim-right"></i>
+                            </button>
+                        </div>
+                     </div>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6">
+                <div class="collections-form">
+                    <div class="form-wrap">
+                        <form action="">
+                            <div class="form-inner">
+                                <p class="news-form-head">Заказать пошив</p>
+                                <input type="text" name="name" placeholder="Имя*">
+                                <input type="text" name="phone" placeholder="Телефон*">
+                                <input type="text" name="massage" placeholder="Сообщение*">
+                                <div class="btn-wrap">
+                                    <button class="btn btn_read_more btn_register">
+                                        <span class="btn_text">Отправить</span>
+                                        <i class="icon-slim-right"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div><!-- // .form-wrap -->
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</section>
+EOD;
+
+$_['text_over_content'] = <<<EOT
+<section class="section section-about-brand">
+    <div class="decor-element decor-element-1"></div>
+    <div class="decor-element decor-element-2"></div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-6 col-sm-12">
+                <div class="section-content-part">
+                     <div class="site-name-block">
+                        <p class="site-name-text">Natasha Soul</p>
+                     </div>
+                     <div class="about-brand-wrap">
+                        <h3 class="about-brand-head">О бренде</h3>
+                        <p class="about-brand-text">Значимость этих проблем настолько очевидна, что постоянное информационно обеспечение нашей деятельности играет важную роль в формировании систем массового участия. Идейные соображения высшего порядка, а также реализация намеченных плановых заданий позволяет выполнять важные задания по разработке позиций, занимаемых участниками в отношении поставленных задач.</p>
+                     </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-xs-9 col-md-offset-0 col-xs-offset-3">
+                <div class="section-imgs-part">
+                     <div class="img-wrap">
+                        <img src="catalog/view/theme/natashasou/images/about_brand_img.jpg" alt="about_brand_img">
+                     </div>
+                     <div class="img-wrap-bg">
+                        <img src="catalog/view/theme/natashasou/images/about_bg_img.jpg" alt="about_bg_img">
+                     </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="section section-about-studio">
+    <div class="decor-element decor-element-1"></div>
+    <div class="decor-element decor-element-2"></div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-5 col-sm-12 col-md-push-7 col-sm-push-0">
+                <div class="studio-content-part equal-height">
+                     <div class="about-studio-wrap">
+                        <h3 class="about-studio-head">Ателье</h3>
+                        <p class="about-brand-text">Значимость этих проблем настолько очевидна, что постоянное информационно обеспечение нашей деятельности играет важную роль в формировании систем массового участия.</p>
+                        <p class="about-brand-text">Идейные соображения высшего порядка, а также реализация намеченных плановых заданий позволяет выполнять важные задания по разработке позиций, занимаемых участниками в отношении поставленных задач. Повседневная практика показывает, что рамки и место обучения кадров играет важную роль в формировании системы обучения кадров, соответствует насущным потребностям. </p>
+                        <p class="about-brand-text">Не следует, однако забывать, что постоянное информационно обеспечение нашей деятельности позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям. Разнообразный и богатый опыт постоянный количественный рост и сфера нашей активности позволяет выполнять важные задания по разработке новых предложений. </p>
+                     </div>
+                </div>
+            </div>
+
+            <div class="col-md-7 col-sm-12 col-md-pull-5 col-sm-pull-0">
+                <div class="studio-imgs-wrp equal-height">
+                    <div class="studio-three-imgs-part">
+                        <div class="three-img-wrap top">
+                           <img src="catalog/view/theme/natashasou/images/about_studio_img_3.jpg" alt="about_studio_img">
+                        </div>
+                        <div class="three-img-wrap right">
+                           <img src="catalog/view/theme/natashasou/images/about_studio_img_1.jpg" alt="about_studio_img">
+                        </div>
+                        <div class="three-img-wrap bottom">
+                           <img src="catalog/view/theme/natashasou/images/about_studio_img_2.jpg" alt="about_studio_img">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="section section-about-us">
+    <div class="decor-element decor-element-1"></div>
+    <div class="decor-element decor-element-2"></div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-6 col-sm-12">
+                <div class="section-content-part">
+                     <div class="about-brand-wrap aboutUs">
+                        <h3 class="about-brand-head aboutUs">О Нас</h3>
+                        <h2 class="aboutUs-title">Интернет магазин тканей для одежды, итальянские ткани в Украине</h2>
+                        <p class="about-brand-text">Значимость этих проблем настолько очевидна, что постоянное информационно обеспечение нашей деятельности играет важную роль в формировании систем массового участия. Идейные соображения высшего порядка, а также реализация намеченных плановых заданий позволяет выполнять важные задания по разработке позиций, занимаемых участниками в отношении поставленных задач.</p>
+                        <p class="about-brand-text">Равным образом реализация намеченных плановых заданий в значительной степени обуславливает создание направлений прогрессивного развития. </p>
+                     </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-xs-9 col-md-offset-0 col-xs-offset-3">
+                <div class="section-imgs-part">
+                     <div class="img-wrap">
+                        <img src="catalog/view/theme/natashasou/images/about_us_img.jpg" alt="about_brand_img">
+                     </div>
+                     <div class="img-wrap-bg">
+                        <img src="catalog/view/theme/natashasou/images/about_bg_img.jpg" alt="about_bg_img">
+                     </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12">
+                <p class="about-brand-text">С другой стороны новая модель организационной деятельности требуют от нас анализа модели развития. Идейные соображения высшего порядка, а также рамки и место обучения кадров требуют от нас анализа существенных финансовых и административных условий. Разнообразный и богатый опыт постоянный количественный рост и сфера нашей активности позволяет выполнять важные задания по разработке новых предложений.</p>
+                <p class="about-brand-text">Равным образом реализация намеченных плановых заданий требуют определения и уточнения направлений прогрессивного развития. Товарищи! новая модель организационной деятельности представляет собой интересный эксперимент проверки соответствующий условий активизации. Повседневная практика показывает, что рамки и место обучения кадров играет важную роль в формировании системы обучения кадров, соответствует насущным потребностям.</p>
+            </div>
+        </div>
+    </div>
+</section>
+EOT;
